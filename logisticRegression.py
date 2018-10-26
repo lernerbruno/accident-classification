f9c990a41f78039f615764d40df035883c310766
BrunoLerner <bru.lerner@gmail.com>
2018-10-26 18:19:59 -0300

    Add all features

--- logisticRegression.py
+++ logisticRegression.py
@@ -25,24 +25,43 @@ def normalizeData(columnsToAdd, feature, test_feature):
 def transformFeatures(train_data, test_data):
     # Generating label Encoders for each feature
     le = LabelEncoder()
-    featuresToEncode = train_data[['CASE_STATE', 'SEX', 'PERSON_TYPE']]
+    featuresToEncode = train_data[[
+        'CASE_STATE', 'SEX', 'PERSON_TYPE', 'SEATING_POSITION', 'RESTRAINT_SYSTEM-USE', 'AIR_BAG_AVAILABILITY/DEPLOYMENT', 'EJECTION', 'EJECTION_PATH', 'EXTRICATION', 'NON_MOTORIST_LOCATION', 'POLICE_REPORTED_ALCOHOL_INVOLVEMENT', 'METHOD_ALCOHOL_DETERMINATION', 'ALCOHOL_TEST_TYPE', 'ALCOHOL_TEST_RESULT', 'POLICE-REPORTED_DRUG_INVOLVEMENT',
+        'METHOD_OF_DRUG_DETERMINATION', 'DRUG_TEST_TYPE', 'DRUG_TEST_TYPE_(2_of_3)', 'DRUG_TEST_TYPE_(3_of_3)', 'HISPANIC_ORIGIN', 'TAKEN_TO_HOSPITAL', 'RELATED_FACTOR_(1)-PERSON_LEVEL', 'RELATED_FACTOR_(2)-PERSON_LEVEL', 'RELATED_FACTOR_(3)-PERSON_LEVEL', 'RACE']]
     featuresToEncode = featuresToEncode.apply(lambda x: le.fit_transform(x))
 
     # Add to final features data the features that doesn't need to be one hot encoded
     age_features = train_data['AGE']
     age_features_test = test_data['AGE']
-    data = pd.concat([age_features],
+
+    drug_test_one = train_data['DRUG_TEST_RESULTS_(1_of_3)']
+    drug_test_one_test = test_data['DRUG_TEST_RESULTS_(1_of_3)']
+
+    drug_test_two = train_data['DRUG_TEST_RESULTS_(2_of_3)']
+    drug_test_two_test = test_data['DRUG_TEST_RESULTS_(2_of_3)']
+
+    drug_test_three = train_data['DRUG_TEST_RESULTS_(3_of_3)']
+    drug_test_three_test = test_data['DRUG_TEST_RESULTS_(3_of_3)']
+
+    data = pd.concat([age_features, drug_test_one, drug_test_two, drug_test_three],
                      axis=1)
-    data_test = pd.concat([age_features_test],
+    data_test = pd.concat([age_features_test, drug_test_one_test, drug_test_two_test, drug_test_three_test],
                           axis=1)
 
     # Now for every feature that need to be one hot encoded, we will do it and check if there
     for featureToEncode in featuresToEncode:
         onehot_feature = pd.get_dummies(train_data[featureToEncode])
         onehot_feature_test = pd.get_dummies(train_data[featureToEncode])
+
+        # This is because some feature will have same possible values, and we dont want to conflict dimensions
+        onehot_feature = onehot_feature.add_prefix(featureToEncode + '_')
+        onehot_feature_test = onehot_feature_test.add_prefix(
+            featureToEncode + '_')
+        if featureToEncode == 'RELATED_FACTOR_(1)-PERSON_LEVEL':
+            print onehot_feature.keys
         columnsToAdd = getColumnsToAdd(
             train_data, test_data, featureToEncode)
-        onehot_feature, onehot_feature_test = normalizeData(columnsToRemove, onehot_feature,
+        onehot_feature, onehot_feature_test = normalizeData(columnsToAdd, onehot_feature,
                                                             onehot_feature_test)
         data = data.join(onehot_feature)
         data_test = data_test.join(onehot_feature_test)
@@ -75,4 +94,4 @@ clf.fit(training_features, label)
 
 
 prediction = clf.predict(test_features).tolist()
-# print prediction
+print prediction
