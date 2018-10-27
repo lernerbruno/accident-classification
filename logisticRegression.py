3f894307ea6be343b02ef6c5127c8828a289c124
BrunoLerner <bru.lerner@gmail.com>
2018-10-26 22:03:00 -0300

    Prediction File generated

--- logisticRegression.py
+++ logisticRegression.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 from sklearn.linear_model import LogisticRegression
-from sklearn.preprocessing import OneHotEncoder, LabelEncoder
+# from sklearn.preprocessing import OneHotEncoder
 
 
 # Checks the different values that train_data and test_data have for each feature
@@ -23,12 +23,10 @@ def normalizeData(columnsToAdd, feature, test_feature):
 
 
 def transformFeatures(train_data, test_data):
-    # Generating label Encoders for each feature
-    le = LabelEncoder()
+    # Defining features we wanna one hot encode
     featuresToEncode = train_data[[
         'CASE_STATE', 'SEX', 'PERSON_TYPE', 'SEATING_POSITION', 'RESTRAINT_SYSTEM-USE', 'AIR_BAG_AVAILABILITY/DEPLOYMENT', 'EJECTION', 'EJECTION_PATH', 'EXTRICATION', 'NON_MOTORIST_LOCATION', 'POLICE_REPORTED_ALCOHOL_INVOLVEMENT', 'METHOD_ALCOHOL_DETERMINATION', 'ALCOHOL_TEST_TYPE', 'ALCOHOL_TEST_RESULT', 'POLICE-REPORTED_DRUG_INVOLVEMENT',
         'METHOD_OF_DRUG_DETERMINATION', 'DRUG_TEST_TYPE', 'DRUG_TEST_TYPE_(2_of_3)', 'DRUG_TEST_TYPE_(3_of_3)', 'HISPANIC_ORIGIN', 'TAKEN_TO_HOSPITAL', 'RELATED_FACTOR_(1)-PERSON_LEVEL', 'RELATED_FACTOR_(2)-PERSON_LEVEL', 'RELATED_FACTOR_(3)-PERSON_LEVEL', 'RACE']]
-    featuresToEncode = featuresToEncode.apply(lambda x: le.fit_transform(x))
 
     # Add to final features data the features that doesn't need to be one hot encoded
     age_features = train_data['AGE']
@@ -52,17 +50,15 @@ def transformFeatures(train_data, test_data):
     for featureToEncode in featuresToEncode:
         onehot_feature = pd.get_dummies(train_data[featureToEncode])
         onehot_feature_test = pd.get_dummies(train_data[featureToEncode])
+        columnsToAdd = getColumnsToAdd(train_data, test_data, featureToEncode)
+        onehot_feature, onehot_feature_test = normalizeData(columnsToAdd, onehot_feature,
+                                                            onehot_feature_test)
 
         # This is because some feature will have same possible values, and we dont want to conflict dimensions
         onehot_feature = onehot_feature.add_prefix(featureToEncode + '_')
         onehot_feature_test = onehot_feature_test.add_prefix(
             featureToEncode + '_')
-        if featureToEncode == 'RELATED_FACTOR_(1)-PERSON_LEVEL':
-            print onehot_feature.keys
-        columnsToAdd = getColumnsToAdd(
-            train_data, test_data, featureToEncode)
-        onehot_feature, onehot_feature_test = normalizeData(columnsToAdd, onehot_feature,
-                                                            onehot_feature_test)
+
         data = data.join(onehot_feature)
         data_test = data_test.join(onehot_feature_test)
 
@@ -93,5 +89,24 @@ clf = LogisticRegression(random_state=0, solver='lbfgs',
 clf.fit(training_features, label)
 
 
-prediction = clf.predict(test_features).tolist()
-print prediction
+classes_numeric_mapping = {
+    'Possible_Injury': 0,
+    'No_Injury': 1,
+    'Incapaciting_Injury': 6,
+    'Fatal_Injury': 3,
+    'Unknown': 4,
+    'Nonincapaciting_Evident_Injury': 5,
+    'Died_Prior_to_Accident': 2,
+    'Injured_Severity_Unknown': 7
+}
+# Create output file with prediction mapping
+output_file = open("prediction.txt", "w")
+predictionList = clf.predict(test_features).tolist()
+index = 1
+for prediction in predictionList:
+    output_file.write(str(classes_numeric_mapping[prediction]))
+    if index < len(predictionList):
+        output_file.write(",")
+    index += 1
+
+output_file.close()
